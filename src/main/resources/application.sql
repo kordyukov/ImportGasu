8b6546a52bb765a3805bdb074866d5348a11afb7
denis.kordyukov <denis.kordyukov@fors.ru>
2023-10-16 16:45:51 +0300

    @

--- src/main/resources/application.sql
+++ src/main/resources/application.sql
@@ -6,16 +6,14 @@ select la.application_number AS "Уникальный номер заявлен�
        nsiwork.erul_name AS "Разрешительный вид деятельности",
        la.registration_date AS "Дата подачи заявления",
        nsitype.code AS "Идентификатор разрешительного органа",
-       con.full_name AS "Разрешительный орган"
-from license.application la,
-            nsi.nsi_application_type nsiapp,
-            nsi.nsi_activity_kind nsiact,
-            nsi.nsi_work_type nsiwork,
-            profile.contragent con,
-            nsi.nsi_contragent_type nsitype
-where la.application_type_id = nsiapp.id
-and nsiact.id = la.activity_kind_id
-and nsiwork.activity_kind_id = la.activity_kind_id
-and la.territory_organ_id = con.id
-and nsitype.id = con.contragent_type_id
-and la.object_deleted = false;
+       con.full_name AS "Разрешительный орган",
+       subcode.code AS "Код субъекта РФ"
+from license.application la
+left join nsi.nsi_application_type nsiapp on la.application_type_id = nsiapp.id
+left join nsi.nsi_activity_kind nsiact on nsiact.id = la.activity_kind_id
+left join nsi.nsi_work_type nsiwork on nsiwork.activity_kind_id = la.activity_kind_id
+left join profile.contragent con on la.territory_organ_id = con.id
+left join nsi.nsi_contragent_type nsitype on nsitype.id = con.contragent_type_id
+left join public.b4_fias_address b4addr on con.legal_address_id = b4addr.id
+left join nsi.nsi_rf_subjects_codes subcode on subcode.id = b4addr.rf_subjects_codes_id
+where la.object_deleted = false;
