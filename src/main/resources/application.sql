0cb65ee71b1c6c132ef9013a3f84933cca7d5fa6
denis.kordyukov <denis.kordyukov@fors.ru>
2023-10-17 10:26:57 +0300

    @

--- src/main/resources/application.sql
+++ src/main/resources/application.sql
@@ -9,7 +9,9 @@ select la.application_number AS "Уникальный номер заявлен�
        con.full_name AS "Разрешительный орган",
        subcode.code AS "Код субъекта РФ",
        subcode.name AS "Субъект РФ",
-       b4addr.id
+       appsubmit.name AS "Способ направления",
+       contype.name AS "Тип заявителя",
+       applicant.full_name AS "Наименование заявителя (для ЮЛ), ФИО для ФЛ и ИП"
 from license.application la
 left join nsi.nsi_application_type nsiapp on la.application_type_id = nsiapp.id
 left join profile.contragent con on la.territory_organ_id = con.id
@@ -17,4 +19,7 @@ left join nsi.nsi_activity_kind nsiact on nsiact.id = la.activity_kind_id
 left join nsi.nsi_work_type nsiwork on nsiwork.activity_kind_id = nsiact.id
 left join public.b4_fias_address b4addr on con.legal_address_id = b4addr.id
 left join nsi.nsi_rf_subjects_codes subcode on b4addr.rf_subjects_codes_id = subcode.id
+left join nsi.nsi_application_submit_method appsubmit on la.delivery_method_id = appsubmit.id
+left join profile.contragent applicant on applicant.id = la.contragent_id
+left join nsi.nsi_contragent_type contype on applicant.contragent_type_id = contype.id
 where la.object_deleted = false limit 100;
