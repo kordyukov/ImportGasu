0f7fd9fb9fbb14aeb024a15089963f3c71f099e3
denis.kordyukov <denis.kordyukov@fors.ru>
2023-10-18 15:16:39 +0300

    @

--- src/main/resources/application.sql
+++ src/main/resources/application.sql
@@ -14,7 +14,7 @@ select distinct on(la.input_application_number) la.input_application_number AS "
        applicant.full_name AS "Наименование заявителя (для ЮЛ), ФИО для ФЛ и ИП",
        applicant.ogrn AS "ОГРН (ОГРНИП) заявителя (для ЮЛ и ИП)",
        applicant.inn AS "ИНН заявителя",
-       b4status.name AS "Решение",
+       (select decision.name from nsi.nsi_decision_result decision where dec.decision_result_id = decision.id) AS "Решение",
        rej.name  AS "Причина отказа",
        la.object_edit_date AS "Дата принятия решения",
        la.application_number AS "Регистрационный номер разрешения",
@@ -32,20 +32,11 @@ left join nsi.nsi_application_submit_method appsubmit on la.delivery_method_id =
 left join profile.contragent applicant on applicant.id = la.contragent_id
 left join nsi.nsi_contragent_type contype on applicant.contragent_type_id = contype.id
 left join public.b4_state b4status on la.state_id = b4status.id
-left join nsi.nsi_decision_result rej on la.error_elimination_decision_id = rej.id
+left join license.decision dec on la.id = dec.application_id
+left join license.decision_refusal_reason refreas on refreas.decision_id = dec.id
+left join nsi.nsi_refusal_reason rej on refreas.refusal_reason_id = rej.id
 where la.object_deleted = false
+  and rej.name is not null
 and la.registration_date between '2023-10-16 00:00:00.000000'
 and '2023-10-16 23:59:59.999999'
 order by la.input_application_number, nsiapp.name, con.full_name
-
-
-select  max(la.registration_date) from license.application la
-select  max(la.object_create_date) from license.application la
-select  max(la.object_edit_date) from license.application la
-select  max(la.create_date) from license.application la
-select  max(la.decide_date) from license.application la
-
-select * from license.application la where state_id = 2837
-
-select * from license.application la where la.input_application_number = '04-0095/С'
-  select * from  public.b4_state b4status where b4status.name = 'Отказ в приеме документов'
